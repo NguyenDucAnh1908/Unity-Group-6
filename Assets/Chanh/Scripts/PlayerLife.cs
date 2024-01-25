771893a839fd39f718ee420d8ff02aa3969fd4b0
chanhtong123 <chanhtong11@gmail.com>
2024-01-26 00:10:08 +0700

    Chanh_LV1-2
    2 Scenes

---
+++ Assets/Chanh/Scripts/PlayerLife.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator _animator;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die() 
+    {
+        _animator.SetTrigger("death");
+    }
+
+    private void RestarLevel() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Kiểm tra nếu vị trí Y của nhân vật là -10
+        if (transform.position.y <= -10f)
+        {
+            // Gọi hàm RestartLevelS
+            RestarLevel();
+        }
+    }
+}
---
+++ Assets/Chanh/Scripts/Wall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private bool isSliding = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Kiểm tra xem đang trượt không
+        if (isSliding)
+        {
+            // Ví dụ: Di chuyển xuống theo trục y
+            rb.velocity = new Vector2(0, -5f);
+        }
+    }
+
+
+    // Xử lý va chạm
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Bắt đầu trạng thái trượt
+            isSliding = true;
+
+            // Disable Collider để không tương tác được
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
